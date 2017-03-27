7b87bdb24ca023c50f32e5091c97e94ebeabecde
varpeti <peti.var97@gmail.com>
2017-03-27 18:35:07 +0200

    Asztali

--- Bevprog2/cpp/work/farkasbarany.cpp
+++ Bevprog2/cpp/work/farkasbarany.cpp
@@ -1,5 +1,5 @@
 #include "graphics.hpp"
-#include "sstream"
+#include "string"
 #include "vector"
 #include "time.h"
 #include "math.h"
@@ -8,33 +8,39 @@
 using namespace genv;
 using namespace std;
 
+// Környezeti konstansok
 const int kx = 1330;
 const int ky = 700;
 const bool teljes = false;
 const int nagyszam = kx+ky;
 const int megevesitav = 10;
+
+// Játék konstansok
 const int egyedf = 10;
 const int egyedb = 50;
 
-const double baranyseb = 3.4;
-const double baranyteh = 60;
+const double baranyseb = 3.4; // Maximum sebesség
+const double baranyteh = 60; // Tehetetlenség
 const double farkasseb = 3.8;
-const double farkasteh = 75;
+const double farkasteh = 75; //Ezekkela konstansokkal játszva lehet kapni érdekes "játékokat:" pl cápák vs halak, ufók vs tehenek stb
 
+//Debug
 bool DEBUG = false;
 
-struct Sboxok
+class Sboxok
 {
+	public:
+
 	double x,y;
 	double vx,vy;
-	double sx,sy;
+	double sx,sy; // Csak hogy szépen menjen ki a képernyőröl (méret)
 	double tehetetlenseg;
 	double maxseb;
 	int szam;
 	unsigned char rr,gg,bb;
 
 	Sboxok (double x, double y, double maxseb, double tehetetlenseg, int szin)
-	: x(x), y(y), sx(10), sy(10), vx(0), vy(0), maxseb(maxseb), tehetetlenseg(tehetetlenseg), szam(0)
+		: x(x), y(y), sx(10), sy(10), vx(0), vy(0), maxseb(maxseb), tehetetlenseg(tehetetlenseg), szam(0)
 	{
 		if (szin<=255) 					{rr=255-(szin-0*255); 	gg=(szin-0*255); 		bb=000;} 				else
 		if (szin>255 and szin<=2*255) 	{rr=000; 				gg=255-(szin-1*255); 	bb=(szin-1*255);} 		else
@@ -43,17 +49,34 @@ struct Sboxok
 
 	void supdate(double mx, double my, bool fele);
 	void srajzol();
+
+	virtual bool farkase() = 0;
+};
+
+struct Sfarkas : Sboxok
+{
+	Sfarkas(double x, double y, int szin) : Sboxok(x,y,farkasseb,farkasteh,szin) {};
+
+	bool farkase() {return true;};
+};
+
+struct Sbarany : Sboxok
+{
+	Sbarany(double x, double y, int szin) : Sboxok(x,y,baranyseb,baranyteh,szin) {};
+
+	bool farkase() {return false;};
 };
 
 void Sboxok::supdate(double mx, double my, bool fele) 
 {	
-	if (mx!=-1) {
+	if (mx!=-1)
+	{	
 		double a = mx-x;
 		double b = my-y;
 		double h = ( sqrt(a*a+b*b) )/(rand()%10+3)*tehetetlenseg; if (h==0) h=1;
 		if (fele) {vx+=a/h;vy+=b/h;}
-		else {vx-=a/h;vy-=b/h;}
-	
+		else {vx-=a/h;vy-=b/h;}		
+
 		if (vx>maxseb) vx=maxseb; else if (vx<-maxseb) vx=-maxseb;
 		if (vy>maxseb) vy=maxseb; else if (vy<-maxseb) vy=-maxseb;
 	}
@@ -76,12 +99,12 @@ void Sboxok::srajzol()
 	// Hogy ki tudjon lógi a képernyőről
 	double ux,uy,usx,usy,ukx,uky;
 	ux=x;uy=y;usx=sx;usy=sy;
-	if (ux+usx<0 or ux>kx or uy+usy<0 or uy>ky) return;
+	if (ux+usx<0 or ux>kx or uy+usy<0 or uy>ky) return; // Ha teljesen kint van
 	if (ux+usx>kx) {usx=kx-ux;}
 	if (ux<0) {usx+=ux; ux=0;}
 	if (uy+usy>ky) {usy=ky-uy;}
 	if (uy<0) {usy+=uy; uy=0;}
-	gout << color(rr,gg,bb) << move_to(ux,uy) << text(to_string(szam));
+	gout << color(rr,gg,bb) << move_to(ux,uy) << text(to_string(szam)); // Csak a számot írjuk ki
 }
 
 double tavolsag(Sboxok *barany,Sboxok *farkas)
@@ -92,55 +115,62 @@ double tavolsag(Sboxok *barany,Sboxok *farkas)
 	return tav;
 }
 
-
-
-void updatedraw(vector<Sboxok*> &farkasok,vector<Sboxok*> &baranyok)
+void ujegyedek(vector<Sboxok*> &egyedek,int f,int b)
 {
-	// Ezt a 2 ciklust lehetne egybe de így átláthatób.
+	for (int i = 0; i < f; ++i) //Létrehzom a bárányokat és a farkasokat
+	{
+		Sboxok *farkas = new Sfarkas(rand()%kx,rand()%ky,rand()%50); // x, y, szín (vörös)
 
-	for (int i = 0; i < farkasok.size(); ++i)
-	{	
-		int min = -1; // A legközelebbi bárány | azért nem mutató mert a megevéshez ID kell;
-		double mtav = nagyszam; //ekkora távolságra nem lehetnek
-		for (int j = 0; j < baranyok.size(); ++j)
-		{
-			double tav = tavolsag(baranyok[j],farkasok[i]);
-			if (tav<mtav) {mtav=tav,min=j;}
-		}
-
-		if (min>-1) // Ha van bárány
-		{
-			if (mtav<megevesitav) { // törli ha megette
-				delete baranyok[min];
-				baranyok[min] = baranyok[baranyok.size()-1];
-				baranyok.pop_back();
-				farkasok[i]->szam++; // Növeli a farkas számlálóját
-			}else
-				farkasok[i]->supdate(baranyok[min]->x,baranyok[min]->y,true); // átadja merre van | true = felé megy
-		}else //Nem talált célt, lehet nincs bárány?
-			if (baranyok.size()==0) 
-				for (int i = 0; i < egyedb; ++i){baranyok.push_back(new Sboxok(rand()%kx,rand()%ky,baranyseb,baranyteh,rand()%50+255));} //új bárányok hozzáadása
-			else
-				farkasok[i]->supdate(-1,-1,true); //ha nincs cél, de még van bárány, eddigi irány folytatása
-
-		farkasok[i]->srajzol();
+		egyedek.push_back(farkas);
 	}
 
-	for (int i = 0; i < baranyok.size(); ++i)
+	for (int i = 0; i < b; ++i)
 	{
-		Sboxok *min = 0; // A legközelebbi farkas
-		double mtav = nagyszam; 
-		for (int j = 0; j < farkasok.size(); ++j)
+		Sboxok *barany = new Sbarany(rand()%kx,rand()%ky,rand()%50+255); //(zöld)
+
+		egyedek.push_back(barany);
+	}
+}
+
+void updatedraw(vector<Sboxok*> &egyedek)
+{
+
+	for (int i = 0; i < egyedek.size();)
+	{	
+		double mintav = nagyszam; //Ennél távolabb nem lehet :D
+		int minId = -1; //azért int és nem mutató mert így könnyebb törölni
+		for (int j = 0; j < egyedek.size(); ++j)
+			if (egyedek[i]->farkase()!=egyedek[j]->farkase()) // Fajtársakkal nem nézünk távolságot.
+			{
+				double tav = tavolsag(egyedek[i],egyedek[j]); // Lényegtelen melyik a farkas
+				if (tav<mintav) {mintav=tav; minId=j;} //Minimum kiválasztás
+			}
+
+		double mx=-1,my=-1; // változatlanul menjen tovább előre.
+
+		if (minId!=-1)
 		{
-			double tav = tavolsag(baranyok[i],farkasok[j]);
-			if (tav<mtav) {mtav=tav,min=farkasok[j];}
+			if (mintav<megevesitav and !egyedek[i]->farkase()) //Barany aki elég közel került egy farkashoz
+			{
+				// Szabályos cserés törlés memória felszabadítással
+				delete egyedek[i];
+				egyedek[i] = egyedek[egyedek.size()-1];
+				egyedek.pop_back();
+
+				egyedek[minId]->szam++; // Farkas frag növelés
+
+				if (egyedek.size()-1==egyedf) ujegyedek(egyedek,0,egyedb);
+
+				continue; // Mivel nem nő a ciklusváltozó a kicserélt egyed se lesz kihagyva
+			}
+			mx=egyedek[minId]->x;
+			my=egyedek[minId]->y;
 		}
 
-		if (min)
-			baranyok[i]->supdate(min->x,min->y,false); // ha van közelben farkas
-		else
-			baranyok[i]->supdate(rand()%kx,rand()%ky,false); // Ha nincs, random mozgás
-		baranyok[i]->srajzol();
+
+		egyedek[i]->supdate(mx,my,egyedek[i]->farkase());
+		egyedek[i]->srajzol();
+		i++; // csak akkor növelem ha végére ért.
 	}
 }
 
@@ -152,22 +182,8 @@ int main()
 
 	//gout.showmouse(false);
 
-	vector<Sboxok*> farkasok;
-	vector<Sboxok*> baranyok;
-
-	for (int i = 0; i < egyedf; ++i) //Létrehzom a bárányokat és a farkasokat
-	{
-		Sboxok *farkas = new Sboxok(rand()%kx,rand()%ky,farkasseb,farkasteh,rand()%50); // x, y, maximum sebesség, tehetetlenség, szín
-
-		farkasok.push_back(farkas);
-	}
-
-	for (int i = 0; i < egyedb; ++i)
-	{
-		Sboxok *barany = new Sboxok(rand()%kx,rand()%ky,baranyseb,baranyteh,rand()%50+255); // x, y, maximum sebesség, tehetetlenség, szín
-
-		baranyok.push_back(barany);
-	}
+	vector<Sboxok*> egyedek;
+	ujegyedek(egyedek,egyedf,egyedb);
 
 	gin.timer(20);
 
@@ -180,20 +196,20 @@ int main()
 				<< move_to(0,0) 
 				<< box(kx,ky);
 
-			updatedraw(farkasok,baranyok);
+			updatedraw(egyedek);
 
 			gout << refresh;
+		}else if (ev.type==ev_key)
+		{
+			DEBUG=!DEBUG;
 		}
 	}
-	while(baranyok.size()>0)
+
+	while(egyedek.size()>0) // takarítás
 	{
-		delete baranyok[baranyok.size()-1];
-		baranyok.pop_back();
+		delete egyedek[egyedek.size()-1];
+		egyedek.pop_back();
 	}
-	while(farkasok.size()>0)
-	{
-		delete farkasok[farkasok.size()-1];
-		farkasok.pop_back();
-	}
+	
 	return 0;
 }
