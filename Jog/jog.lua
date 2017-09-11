9835384e34707026754baa88dcbdd24224b0b36c
Váraljai Péter <peti.var97@gmail.com>
2017-09-11 09:59:29 +0200

    Jog

---
+++ Jog/jog.lua
@@ -0,0 +1,18 @@
+"Honeste vivere, alterum non laedere, suum cuique tribuere"
+
+"To live honestly, to injure no one, [and] to give to each his own"
+
+4 előírás kategória:
+	-Erkölcsi
+	-Vallási
+		Sokszor nem különíthető el az erkölcsitől
+	-Etika, illem, szokás
+	-Jogi normák
+		Szankciók
+
+
+Hazugság
+	Jogi szempotból számos változata létezik
+		Családon belüli hazugságnak nincs szankciója,
+		De hamis tanúskodásért komoly következmények várhatóak.
+		Sajtóba is lehet hazudni, bizonyos fokig.
