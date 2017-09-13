2384016ae389e69fe5c306d6c88969c29a0025d1
Váraljai Péter <peti.var97@gmail.com>
2017-09-13 21:46:54 +0200

    Adatszerk
    Uncsi ismétlés

---
+++ Adatszerk/adatszerk.lua
@@ -0,0 +1,15 @@
+--[[17.09.12
+]]
+
+ZH = {10.24,12.13,12.20}
+
+Adatszerkezetek
+Algoritmusok
+Hatékonyság
+
+Típus: egy adat által felvehető értékek és az adaton értelmezett műveletek halmaza
+
+	Specifikáció:
+		típus érték halmaz
+		típus műveletek
+		
