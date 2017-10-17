338f13109337cb3f88c8ac46085e214c33629c5d
Váraljai Péter <peti.var97@gmail.com>
2017-10-17 20:53:45 +0200

    aramkorok hf01

--- Aramkorok/hf02/hf01.lua
+++ Aramkorok/hf02/hf01.lua
@@ -1,10 +1,3 @@
-sqrt = math.sqrt
-
-t={}
-i={}
-A={}
-s={}
-
 --[[                        U0=10V
         +--(L=5H->)--(C=100uF->)--o
         |                          / K1: t(0)=zár
@@ -12,32 +5,22 @@ s={}
 
 --]]
 
---[[
-    Másodrendű
-    Állandósult állapotbeli megoldás = 0
-    Két kezdeti feltétel
-]]
-
 R=10        --Ohm
 L=5         --H
 C=100e-6    --F
-
---Kezdeti feltételek:
-U0=10
-i[0]=0
-
---Kirchhoff:
+U0=10       --V
 
 --[[
-    V["R"]+V["L"]+V["C"]=0
+    Hurok törvény:
+    U["R"]+U["L"]+U["C"]-V0=0
     
     ebből:
 
-    +R*i+U0 +dif(L[1],i,1) -V[0]+(1/C[1])*int(0,t,i,r)
+    +R*i  +dif(L[1],i,1) +(1/C[1])*int(0,t,i,r) -V0 = 0
 
     ebből diffegyenlet:
 
-    (1/C[1])*i +diff(R[1],i,1) +diff(L[1],i,2)
+    (1/C[1])*i +diff(R[1],i,1) +diff(L[1],i,2) = 0
 --]]
 
 --[[
@@ -51,16 +34,43 @@ i[0]=0
 
     A*e^(s*t)*(1/C + s*R + s^2*L) = 0
 
-    s[1]= (-R+sqrt(R^2-(4*L)/C)) / (2*L)
-    s[2]= (-R-sqrt(R^2-(4*L)/C)) / (2*L)
+    1/C + s*R + s^2*L = 0       | másodfokó
 
-    i = A[1]*e^(s[1]*t) + A[2]*e^(s[2]*t)
+    s[1]= ( -R+sqrt(R^2-4*L*(1/C)) ) / (2*L)
+    s[2]= ( -R-sqrt(R^2-4*L*(1/C)) ) / (2*L)
 
+    s[1]= -1+44.710j
+    s[2]= -1-44.710j
+
+--]]
+
+--[[
+    Komplex megoldás:
+    i = A*e^(-w) * sin(w*t+Đ)
+
+    Behelyettesítve:
+
+    i = A*e^(-t) * sin(44.710*t+Đ)
+--]]
+
+--[[
+    Kezdeti feltételek:
+
+    t=0
+    i[L]=0
+    V[L]=V0=L*(di/dt)
+
+    t=0 -> i=0 = A*e^0*sin(Đ)  | ha A véges akkor Đ=0
+    V0 = L*(di/dt) = L (-1*A*e^0*sin(0) + 44.710*A*e^0*cos(0))
+
+    A = 10/(5*44.710) = 0.447 
+--]]
+
+--[[
+    Megoldás:
+    i = 0.447*e^(-t)*sin(44.7*t)
 ]]
 
---Behelyettesítés
-s[1]= (-R+sqrt(R^2-(4*L)/C)) / (2*L)
-s[2]= (-R-sqrt(R^2-(4*L)/C)) / (2*L) --Komplex számok
 
 
 
