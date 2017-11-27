b245ebb522d8ca8b914881d9708fc4ff74987e20
varpeti <peti.var97@gmail.com>
2017-11-27 22:35:52 +0100

    Asztali adatszerk tutuk

--- Adatszerk/hazi/varpe8/homokozo/hf03_per0_teszt/main.cpp
+++ Adatszerk/hazi/varpe8/homokozo/hf03_per0_teszt/main.cpp
@@ -1,39 +1,64 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include "hash.hpp"
 
 using namespace std; 
 
-bool contain_p0(string str) // False ha tartalmaz \0-at
+bool van_benne_p0(string str)
 {
     for (unsigned int i = 0; i < str.size(); ++i)
     {      
-        if ( str.at(i)=='\0' ) return false;
+        if ( str.at(i)=='\0' ) return true;
     }
-    return true;
+    return false;
+}
+
+void print(string str,string nev)
+{
+	cout << nev << ":\n\ttartalma: " << str << "\n\tvan benne \\0: " << van_benne_p0(str) << "\n\tmerete: " << str.size() << endl;
+}
+
+void egyenlo(string a, string b, string an, string bn)
+{
+	cout << "\t" << an << "==" << bn << ": " << (a==b) << endl; 
 }
 
 
 int main()
-{	string a1="q",a2="qq";
-	string alma = "";
-	alma += a1 + '\0'; // A alma += "q" + '\0' Nem működik!!!
+{	
+	string a1="q",a2="qq";
+
+	string alma = ""; // Jó módszer
+	alma += a1 + '\0';
 	alma += a2 + '\0';
-	string korte = "";
+	
+	string korte = ""; //Rossz
 	korte += a2 + '\0';
 	korte += a1 + '\0';
 
+	string banan = "q\0qq\0"; //Rossz
 
-	cout << alma << " " << korte << endl;
-	cout << contain_p0(alma) << " " << alma.size() << endl;
-	cout << (alma==korte) << endl;
-	HASH hash;
-	hash.set(alma,0);
-	cout << hash.get(alma) << endl;
-	cout << hash.get(korte) << endl;
+	string szilva = "q"; // Jó
+	szilva.push_back('\0');
+	szilva += "qq";
+	szilva.push_back('\0');
 
-	getchar();
-	
+	string kivi = ""; //Rossz
+	kivi += "q" + '\0'; //azért mert a "q" az igazából char[] és nem string
+	kivi += "qq" + '\0';
+
+	//Adatok kiírása
+	print(alma,"alma");
+	print(korte,"korte");
+	print(banan,"banan");
+	print(szilva,"szilva");
+	print(kivi,"kivi");
+
+	//Egyenlőség teszt
+	cout << endl << "Egyenloseg:\n" << endl;
+	egyenlo(alma,korte,"alma","korte");
+	egyenlo(alma,banan,"alma","banan");
+	egyenlo(alma,szilva,"alma","szilva");
+	egyenlo(alma,kivi,"alma","kivi");
+
 	return 0;
 }
