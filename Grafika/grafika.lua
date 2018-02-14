d73bf5e519a59ca35ad2e88fec767651d80ee36d
Váraljai Péter <peti.var97@gmail.com>
2018-02-14 12:51:04 +0100

    Laptop datab

---
+++ Grafika/grafika.lua
@@ -0,0 +1,30 @@
+--[[ 02.13
+]]
+Honlapok:
+    grafika.itk.ppke.hu
+    mplab.sztaki.hu/~bcsaba
+
+Lézerszkennelés
+    pontmegjelenés
+
+Óra
+    Előad: 1 hetente 2
+    Gyak:  2 hetente 2
+
+Követelmény
+    1 nagy ZH
+    11 előadás
+    6 beugró
+    Jegyzőkönyvek
+    Írásbeli/Szóbeli vizsga
+
+Aláírás:
+    Órai munka
+    Nagy ZH ápr 24 (1 pót zh)
+
+    vagy
+
+    3db 2D 3D feladat feb 27-ig 
+
+-------------
+
