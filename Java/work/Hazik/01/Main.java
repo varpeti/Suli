e3ff5cc3786eafec6c8090bde6a71fd501c489a0
varpeti <peti.var97@gmail.com>
2018-02-18 15:24:58 +0100

    Asztali HF01, -.class

--- Java/work/Hazik/01/Main.java
+++ Java/work/Hazik/01/Main.java
@@ -5,10 +5,39 @@ import Merleg.Merleg;
 
 public class Main
 {
+    public static void kiir(Merleg merleg,int suly)
+    {
+        int[] megoldas = merleg.sulyokraValt(suly);
+        System.out.print("Targy: "+suly+" Felhasznált súlyok: [ ");
+        for (int i=0;i<megoldas.length-1;i++) System.out.print(megoldas[i]+" ");
+        System.out.println("] összesen: "+megoldas[megoldas.length-1]);
+    }
+
+
     public static void main(String[] args) 
     {
-        Integer[] i = {1,5,20,25};
-        Merleg m = new Merleg(i);
-        m.sulyokraValt(42);
+        {   //A trükkös
+            Integer[] sulyok = {1,5,20,25};
+            Merleg merleg = new Merleg(sulyok);
+            kiir(merleg,42);
+            kiir(merleg,44);
+        }
+
+        {   //Nagy számok
+            Integer[] sulyok = {1,2,5,10,20,50,100,200};
+            Merleg merleg = new Merleg(sulyok);
+            kiir(merleg,231);
+            kiir(merleg,247);
+        }
+
+        {   //Nincs 1
+            Integer[] sulyok = {2,3,5,7};
+            Merleg merleg = new Merleg(sulyok);
+            kiir(merleg,11);
+            kiir(merleg,15);
+        }
+        
+
+        
     }
 }
--- Java/work/Hazik/01/Merleg.java
+++ Java/work/Hazik/01/Merleg.java
@@ -3,50 +3,73 @@ package Merleg;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import Matek.Komplex;
-
 public class Merleg {
     private ArrayList<Integer> sulyok = new ArrayList<Integer>(); //Sulyokat tárolja
 
     public int[] sulyokraValt(int t)
     {
-        ArrayList<Komplex> ka       = new ArrayList<Komplex>(); 
-        ArrayList<Komplex> megoldas = new ArrayList<Komplex>();
 
-        ka.add(new Komplex(t,0));
+        //Adat készítés:
 
-        while (ka.size()>0) {
-            Komplex k = ka.get(ka.size()-1);
+        ArrayList<Integer[]> ka         = new ArrayList<Integer[]>(); //3: suly, maradek, db
+        ArrayList<Integer[]> megoldasok = new ArrayList<Integer[]>(); //3: suly, maradek, db
+
+        ka.add(new Integer[]{0,t,0});
+
+        while (ka.size()>0) { //Rekurzió helyett
+            Integer[] k = ka.get(ka.size()-1);
             ka.remove(ka.size()-1);
-            megoldas.add(new Komplex(maradek,k.get) );
-
-            int maradek = (int)Math.floor(k.getRe());
-            int db = (int)Math.floor(k.getIm());
+            megoldasok.add(k);
 
             for (int i=0;i<sulyok.size();i++) 
             {
                 int suly = sulyok.get(i);
-                if (maradek<suly) continue; //Ha kissebb mint a suly akkor off.
-                Komplex kk = new Komplex(maradek%suly,maradek/suly+db); System.out.println("kk: "+kk);
+                if (k[1]<suly) continue; //Ha kissebb mint a suly akkor off.
+                Integer[] kk = new Integer[]{suly,k[1]%suly,k[1]/suly+k[2]};
                 boolean off = false;
                 for (int j=0;j<ka.size();j++) 
                 {
-                    if  ((int)ka.get(j).getRe()    == (int)kk.getRe() &&                      //Ha benne van
-                         (int)ka.get(j).getIm()+db <  (int)kk.getIm()  ) {off=true; break;}   //és nagyobb, akkor off. 
+                    if  (ka.get(j)[1]      == kk[1] &&                      //Ha benne van
+                         ka.get(j)[2]+k[2] <  kk[2]  ) {off=true; break;}   //és nagyobb, akkor off. 
                     
                 }
-                if ( (int)kk.getRe()==0 ) {off=true;  megoldas.add(new Komplex(maradek,kk.getIm()) );}
-                //megoldas.add(kk);
+                if ( kk[1]==0 ) {off=true; megoldasok.add(kk);}
                 if (off) continue;
                 ka.add(kk);
                 
             }
         }
 
-        System.out.println(megoldas);
+        //Adat feldolgozás:
 
-        int[] i = {1,2,3};
-        return i;
+        //Minimum keresés       // (ha [1]==0 akkor megoldás, és a legjobb megoldás, ha ekkor [2] a minimum)
+        Integer[] min = megoldasok.get(0);
+        int id = 0;
+        for (int i=1;i<megoldasok.size();i++) if (megoldasok.get(i)[1]==0 && (megoldasok.get(i)[2]<min[2] || min[2]==0) ) { min=megoldasok.get(i); id=i;}
+
+        /*Adat kiírás:
+        for (int i=0;i<megoldasok.size();i++) {
+            for (int j=0;j<3;j++) {
+                System.out.print(megoldasok.get(i)[j]+" ");
+            }
+           System.out.println();
+        }
+        System.out.println("min: "+min[0]+" "+min[1]+" "+min[2]+" id: "+id);
+        //*/
+
+        //Visszafejtés a minimumtól
+        int[] megoldas = new int[sulyok.size()+1];
+        megoldas[megoldas.length-1]=megoldasok.get(id)[2];
+        for (int i=id-1;i>=0;i--) 
+        {   
+            if (megoldasok.get(i)[1]>=megoldasok.get(id)[0]) 
+            {
+                for (int j=0;j<sulyok.size();j++) if ((int)sulyok.get(j)==(int)megoldasok.get(id)[0]) {megoldas[j]+=megoldasok.get(i)[1]/megoldasok.get(id)[0]; break; } // (int) nélkül random néha jó volt néha nem... vicc(es) ez a nyelv.
+                id=i;
+            }
+        }
+
+        return megoldas;
     }
 
     public Merleg(Integer[] _sulyok)
