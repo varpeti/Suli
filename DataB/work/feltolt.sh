4263bbe8f730a3c796078690108e373b1417f327
varpeti <peti.var97@gmail.com>
2018-04-03 13:39:19 +0200

    Asztali DataB sql query

--- DataB/work/feltolt.sh
+++ DataB/work/feltolt.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-./merge.sh
-
 echo "idejonajelszo" > fall.sql
-cat all.sql >> fall.sql
+cat $1 >> fall.sql
 
-/home/itep/Programok/Oracle/sqlcl/bin/sql DB18_VARPET@//dboracle.itk.ppke.hu:1521/lab.itk.ppke.hu <fall.sql >out.txt
+/home/itep/Programok/Oracle/sqlcl/bin/sql DB18_VARPET@//dboracle.itk.ppke.hu:1521/lab.itk.ppke.hu <fall.sql
 
 rm fall.sql
